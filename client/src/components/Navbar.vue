8821246095c43460e1be33aa100db4d84ec30c56
paranerd <paranerd.development@gmail.com>
2021-08-22 15:41:27 +0200

    Added navbar

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="nav-wrapper">
+    <span>IoT-Manager</span>
+    <div class="username">paranerd</div>
+  </div>
+  <!--<div id="nav"><router-link to="/">Home</router-link></div>-->
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.nav-wrapper {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 5rem;
+  padding: 1.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  color: white;
+  background: #001f3f;
+  box-sizing: border-box;
+}
+
+.username {
+  margin-left: auto;
+}
+</style>
